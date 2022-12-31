4f59974d70af6f549c8b82bd568eabb06d519118
Youngtaek Yoon <noreply@yoon.anonaddy.me>
2022-12-31 08:42:38 +0000

    Create stub query server

--- x/composable/keeper/grpc_query.go
+++ x/composable/keeper/grpc_query.go
@@ -1,7 +1,7 @@
 package keeper
 
 import (
-	// "context"
+	"context"
 
 	// sdk "github.com/line/lbm-sdk/types"
 	// sdkerrors "github.com/line/lbm-sdk/types/errors"
@@ -10,9 +10,6 @@ import (
 
 type queryServer struct {
 	keeper Keeper
-
-	// TODO: remove it!
-	composable.UnimplementedQueryServer
 }
 
 var _ composable.QueryServer = (*queryServer)(nil)
@@ -23,4 +20,46 @@ func NewQueryServer(keeper Keeper) composable.QueryServer {
 	return &queryServer{
 		keeper: keeper,
 	}
+}
+
+// Params queries the module params.
+func (s queryServer) Params(c context.Context, req *composable.QueryParamsRequest) (*composable.QueryParamsResponse, error) {
+	d := composable.UnimplementedQueryServer{}
+	return d.Params(c, req)
+}
+
+// Class queries an NFT class based on its id
+func (s queryServer) Class(c context.Context, req *composable.QueryClassRequest) (*composable.QueryClassResponse, error) {
+	d := composable.UnimplementedQueryServer{}
+	return d.Class(c, req)
+}
+
+// Classes queries all NFT classes
+func (s queryServer) Classes(c context.Context, req *composable.QueryClassesRequest) (*composable.QueryClassesResponse, error) {
+	d := composable.UnimplementedQueryServer{}
+	return d.Classes(c, req)
+}
+
+// NFT queries an NFT based on its class and id.
+func (s queryServer) NFT(c context.Context, req *composable.QueryNFTRequest) (*composable.QueryNFTResponse, error) {
+	d := composable.UnimplementedQueryServer{}
+	return d.NFT(c, req)
+}
+
+// NFTs queries all NFTs of a given class
+func (s queryServer) NFTs(c context.Context, req *composable.QueryNFTsRequest) (*composable.QueryNFTsResponse, error) {
+	d := composable.UnimplementedQueryServer{}
+	return d.NFTs(c, req)
+}
+
+// Owner queries the owner of the NFT based on its class and id, same as ownerOf in ERC721
+func (s queryServer) Owner(c context.Context, req *composable.QueryOwnerRequest) (*composable.QueryOwnerResponse, error) {
+	d := composable.UnimplementedQueryServer{}
+	return d.Owner(c, req)
+}
+
+// Parent queries the parent of the NFT based on its class and id
+func (s queryServer) Parent(c context.Context, req *composable.QueryParentRequest) (*composable.QueryParentResponse, error) {
+	d := composable.UnimplementedQueryServer{}
+	return d.Parent(c, req)
 }
