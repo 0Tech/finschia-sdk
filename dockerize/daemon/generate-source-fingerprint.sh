74b180aaf3f098e38fd58c1d93ab0d502ec77a21
Youngtaek Yoon <noreply@yoon.anonaddy.me>
2023-05-19 16:00:22 +0900

    Add upgrade test

--- dockerize/daemon/generate-source-fingerprint.sh
+++ dockerize/daemon/generate-source-fingerprint.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+# path for the checksum
 rel_output=$1
 [ -n "$rel_output" ]
 output=$(realpath $rel_output)
@@ -9,8 +10,8 @@ output=$(realpath $rel_output)
 source_dir=$2
 [ -n "$source_dir" ]
 
-dockerizer=$3
-[ -n "$dockerizer" ]
+binary_dir=$3
+[ -n "$binary_dir" ]
 cd $source_dir
 
-tar -c $(ls -A1 | grep -Ev '^('$dockerizer'|\.git)$' | sort) | sha512sum | awk '{print $1}' >$output
+tar -c $(ls -A1 | grep -Ev '^('$binary_dir'|\.git)$' | sort) | sha512sum | awk '{print $1}' >$output
